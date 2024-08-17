14bbeb956974398a3ac2032e841eaf2e713f1c86
Minjoo Kim <haileykim206@gmail.com>
2024-08-17 00:52:42 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import WeatherBox from "./component/WeatherBox";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,13 +13,13 @@ import WeatherButton from "./component/WeatherButton";
 
 function App() {
   const [weather, setWeather] = useState(null);
-  const getCurrentLocation = () => {
+  const getCurrentLocation = useCallback(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
       getWeatherByCurrentLocation(lat, lon);
     });
-  };
+  }, []);
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=bee515905d06c4bede361bb1ed411794&units=metric`;
@@ -31,7 +31,7 @@ function App() {
 
   useEffect(() => {
     getCurrentLocation();
-  }, []);
+  }, [getCurrentLocation]);
 
   return (
     <div>
